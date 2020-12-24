92635cad7a62023559628d49e335a94d7c4dc2a8
Docheinstein <dottorestefano97@gmail.com>
2020-12-24 16:10:55 +0100

    Version 0.12

--- docs/sphinx/src/mans/conf.py
+++ docs/sphinx/src/mans/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Stefano Dottore'
 author = 'Stefano Dottore'
 
 # The full version, including alpha/beta/rc tags
-release = "0.11"
+release = "0.12"
 
 
 # -- General configuration ---------------------------------------------------
